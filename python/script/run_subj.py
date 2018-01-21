c096e0f8c7a4a2dd554a49d0f9f8bf989fc9e8ca
Zhi Li <zli@netflix.com>
2018-01-20 17:34:50 -0800

    Update run_subj script to only plot relevant data.

--- python/script/run_subj.py
+++ python/script/run_subj.py
@@ -11,7 +11,7 @@ from sureal.config import DisplayConfig
 __copyright__ = "Copyright 2016-2018, Netflix, Inc."
 __license__ = "Apache, Version 2.0"
 
-SUBJECTIVE_MODELS = ['MLE', 'MOS', 'DMOS', 'DMOS_MLE', 'MLE_CO', 'DMOS_MLE_CO', 'SR_DMOS', 'SR_MOS', 'ZS_SR_DMOS', 'ZS_SR_MOS']
+SUBJECTIVE_MODELS = ['MOS', 'MLE', 'MLE_CO', 'DMOS', 'DMOS_MLE', 'DMOS_MLE_CO', 'SR_MOS', 'ZS_SR_MOS', 'SR_DMOS', 'ZS_SR_DMOS']
 
 def print_usage():
     print "usage: " + os.path.basename(sys.argv[0]) + " subjective_model dataset_filepath [--output-dir output_dir]\n"
@@ -31,6 +31,12 @@ def main():
 
     output_dir = get_cmd_option(sys.argv, 3, len(sys.argv), '--output-dir')
 
+    do_plot = ['raw_scores', 'quality_scores']
+    if subjective_model in ['MLE', 'MLE_CO', 'DMOS_MLE', 'DMOS_MLE_CO']:
+        do_plot.append('subject_scores')
+    if subjective_model in ['MLE', 'DMOS_MLE']:
+        do_plot.append('content_scores')
+
     try:
         subjective_model_class = SubjectiveModel.find_subclass(subjective_model)
     except Exception as e:
@@ -45,12 +51,7 @@ def main():
         dataset_filepath=dataset_filepath,
         subjective_model_classes = [subjective_model_class,],
         normalize_final=False, # True or False
-        do_plot=[
-            'raw_scores',
-            'quality_scores',
-            'subject_scores',
-            'content_scores',
-        ],
+        do_plot=do_plot,
         plot_type='errorbar',
         gradient_method='simplified',
     )
