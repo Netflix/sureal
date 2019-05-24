Fix ThurstoneMlePairedCompSubjectiveModel test case by normalization.

sureal/pc_subjective_model.py
@@ -283,8 +283,17 @@ class ThurstoneMlePairedCompSubjectiveModel(PairedCompSubjectiveModel):
 
         ret = minimize(nllf_partial, v0, method='SLSQP', jac='2-point', options={'ftol': 1e-8, 'disp': True, 'maxiter':1000})
         assert ret.success, "minimization is unsuccessful."
+        scores = ret.x
 
-        result = {'quality_scores': ret.x, 'quality_scores_std': None}
+        zscore_output = kwargs['zscore_output'] if 'zscore_output' in kwargs and 'zscore_output' is not None else False
+
+        if zscore_output:
+            scores_mean = np.mean(scores)
+            scores_std = np.std(scores)
+            scores = (scores - scores_mean) / scores_std
+            # std = std / scores_std
+
+        result = {'quality_scores': scores, 'quality_scores_std': None}
         return result
 
     @staticmethod

test/pc_subjective_model_test.py
@@ -39,10 +39,12 @@ class PcSubjectiveModelTest(unittest.TestCase):
 
     def test_thrustone_mle_subjective_model(self):
         subjective_model = ThurstoneMlePairedCompSubjectiveModel(self.pc_dataset_reader)
-        result = subjective_model.run_modeling()
-        self.assertAlmostEquals(float(np.sum(result['quality_scores'])), 0.0003982322159217233, places=4)
-        self.assertAlmostEquals(float(np.var(result['quality_scores'])), 0.8580215948887912, places=4)
+        result = subjective_model.run_modeling(zscore_output=True)
+        self.assertAlmostEquals(float(np.sum(result['quality_scores'])), 0.0, places=4)
+        self.assertAlmostEquals(float(np.var(result['quality_scores'])), 1.0, places=4)
         self.assertAlmostEqual(st.kurtosis(result['quality_scores']), -0.3411839039618667, places=4)
+        self.assertAlmostEquals(result['quality_scores'][0], 0.3791409047569019, places=4)
+        self.assertAlmostEquals(result['quality_scores'][-1], -0.41006265745757303, places=4)
         self.assertTrue(result['quality_scores_std'] is None)
 
 