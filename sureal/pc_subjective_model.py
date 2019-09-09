Fix sign bug in CI calculation for ThurstoneMlePairedCompSubjectiveModel; update TYPE name.

sureal/pc_subjective_model.py
@@ -99,7 +99,7 @@ class BradleyTerryNewtonRaphsonPairedCompSubjectiveModel(PairedCompSubjectiveMod
 
     """
 
-    TYPE = 'BTNR'
+    TYPE = 'BT_NR'
     VERSION = '1.0'
 
     @classmethod
@@ -182,7 +182,7 @@ class BradleyTerryMlePairedCompSubjectiveModel(PairedCompSubjectiveModel):
     """ Bradley-Terry model based on maximum likelihood estimation, classical version.
     """
 
-    TYPE = 'BTMLE'
+    TYPE = 'BT_MLE'
     VERSION = '1.0'
 
     @classmethod
@@ -277,7 +277,8 @@ class ThurstoneMlePairedCompSubjectiveModel(PairedCompSubjectiveModel):
 
     TYPE = 'THURSTONE_MLE'
     # VERSION = '1.0'
-    VERSION = '1.1' # fix sign nllf sign issue
+    # VERSION = '1.1'  # fix sign nllf sign issue
+    VERSION = '1.2'  # add confidence interval
 
     @classmethod
     def _run_modeling(cls, dataset_reader, **kwargs):
@@ -323,12 +324,12 @@ class ThurstoneMlePairedCompSubjectiveModel(PairedCompSubjectiveModel):
         f_vj_m_vi   =   f_vi_m_vj
         d_vj_m_vi   = - d_vi_m_vj
         lbda_ii = np.sum(
-            alpha   * (phi_vi_m_vj * d_vi_m_vj - f_vi_m_vj ** 2) / phi_vi_m_vj ** 2 -
+            alpha   * (phi_vi_m_vj * d_vi_m_vj - f_vi_m_vj ** 2) / phi_vi_m_vj ** 2 +
             alpha.T * (phi_vj_m_vi * d_vj_m_vi - f_vj_m_vi ** 2) / phi_vj_m_vi ** 2
         , axis=1)  # summing over axis=1 marginalizes j
 
         lbda_ij = -(
-            alpha   * (phi_vi_m_vj * d_vi_m_vj - f_vi_m_vj ** 2) / phi_vi_m_vj ** 2 -
+            alpha   * (phi_vi_m_vj * d_vi_m_vj - f_vi_m_vj ** 2) / phi_vi_m_vj ** 2 +
             alpha.T * (phi_vj_m_vi * d_vj_m_vi - f_vj_m_vi ** 2) / phi_vj_m_vi ** 2
         )
 

test/pc_subjective_model_test.py
@@ -48,9 +48,11 @@ class PcSubjectiveModelTest(unittest.TestCase):
         self.assertAlmostEqual(st.kurtosis(result['quality_scores']), -0.3411839039618667, places=4)
         self.assertAlmostEquals(result['quality_scores'][0], 0.3791409047569019, places=4)
         self.assertAlmostEquals(result['quality_scores'][-1], -0.41006265745757303, places=4)
-        self.assertAlmostEquals(result['quality_scores_std'][0], 0.23077713530138128, places=4)
-        import math
-        self.assertTrue(math.isnan(result['quality_scores_std'][-1]))  # TODO: fix
+        self.assertAlmostEquals(float(np.sum(result['quality_scores_std'])), 4.893685013444614, places=4)
+        self.assertAlmostEquals(float(np.var(result['quality_scores_std'])), 0.0001601422716472649, places=4)
+        self.assertAlmostEqual(st.kurtosis(result['quality_scores_std']), -0.6892814746512612, places=4)
+        self.assertAlmostEquals(result['quality_scores_std'][0], 0.12571225814158712, places=4)
+        self.assertAlmostEquals(result['quality_scores_std'][-1], 0.11003033360622685, places=4)
 
 
 class PcSubjectiveModelTestSynthetic(unittest.TestCase):